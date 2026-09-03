1d1a179e99431d36544a6358e2b523830037654d
anayethasan <mdneloy256@gmail.com>
2026-09-03 23:43:44 +0600

    user settings and jwt system added

---
+++ app/core/security.py
@@ -0,0 +1,60 @@
+from datetime import datetime, timedelta, timezone
+from uuid import UUID
+
+from jose import JWTError, jwt
+from pwdlib import PasswordHash
+
+from app.core.config import settings
+
+password_hash = PasswordHash.recommended()
+
+def hash_password(password: str) -> str:
+    return password_hash.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return password_hash.verify(
+        plain_password,
+        hash_password,
+    )
+    
+    
+def create_access_token(user_id: UUID) -> str:
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    payload = {
+        "sub": str(user_id),
+        "type": "access",
+        "exp": expire,
+    }
+    
+    return jwt.encode(
+        payload,
+        settings.JWT_SECRET_KEY,
+        algorithm=settings.JWT_ALGORITHM
+    )
+    
+def create_refresh_token(user_id: UUID) -> str:
+    expire = datetime.now(timezone.utc) + timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS)
+    
+    payload = {
+        "sub": str(user_id),
+        "type": "refresh",
+        "exp": expire,
+    }
+    
+    return jwt.encode(
+        payload,
+        settings.JWT_SECRET_KEY,
+        algorithm=settings.JWT_ALGORITHM,
+    )
+    
+def decode_token(token: str) -> dict:
+    try:
+        payload = jwt.decode(
+            token,
+            settings.JWT_SECRET_KEY,
+            algorithms=[settings.JWT_ALGORITHM],
+        )
+        return payload
+    except JWTError:
+        raise ValueError("Invalid or expired token")
